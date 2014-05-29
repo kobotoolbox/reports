59bd08eb11e657be56f9fe4f0d9aac0d0218d246
Andrew Marder <andrew.n.marder@gmail.com>
2014-05-29 07:10:07 -0400

    Moved over to compilation process driven by config file.

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
-data/mortality_rates.csv:
+data:
 	mkdir data
-	curl -o data/worldbank.zip "http://api.worldbank.org/v2/en/indicator/sh.dyn.mort?downloadformat=csv"
-	cd data; unzip worldbank.zip
-	mv data/sh.dyn.mort_Indicator_en_csv_v2.csv data/mortality_rates.csv
-	touch data/mortality_rates.csv
+
+data/games.csv: data
+	curl -o data/games.csv http://www.calvin.edu/~stob/data/bballgames03.csv
+	touch data/games.csv
+
+data/mortality_rates.csv: data
+	mkdir temp
+	curl -o temp/worldbank.zip "http://api.worldbank.org/v2/en/indicator/sh.dyn.mort?downloadformat=csv"
+	cd temp; unzip worldbank.zip
+	cat temp/sh.dyn.mort_Indicator_en_csv_v2.csv | sed '1,2d' > data/mortality_rates.csv
+	rm -r temp
 
 clean:
 	rm -rf data reports figure mortality.md
@@ -14,4 +21,4 @@ REPORTS = $(addsuffix .html,$(addprefix reports/,$(COUNTRY_CODES)))
 reports: $(REPORTS)
 
 reports/%.html: compiler.R mortality.Rmd data/mortality_rates.csv
-	Rscript compiler.R "destination <- '$@'"
+	Rscript compiler.R "destination <- '$@'" "suspicious.time <- 10"
--- compiler.R
+++ compiler.R
@@ -1,18 +1,50 @@
 library(knitr)
+library(rjson)
+library(pander)
+library(ggplot2)
 
-args <- commandArgs(trailingOnly = TRUE)
-for (line in args) {
-    # expects destination
-    eval(parse(text=line))
+# TODO: It would be good to tap into a good SQL library here.
+extract <- function(path, ...) {
+    options(stringsAsFactors=FALSE)
+    result <- read.csv(path)
+    l <- list(...)
+    for (k in names(l)) {
+        keep <- result[[k]] %in% l[[k]]
+        result <- result[keep, ]
+    }
+    return(result)
 }
 
-# Create an HTML report using knitr.
-country.code <- gsub('(reports/|.html)', '', destination)
-knit2html('mortality.Rmd', quiet=TRUE)
-system('mkdir -p reports')
-system(paste('mv mortality.html', destination))
+set.knitr.defaults <- function() {
+    opts_chunk$set(error=FALSE, echo=FALSE)
+}
 
-# Let's also make a pdf using pandoc.
-system('pandoc mortality.md -o mortality.pdf')
-pdf.path <- paste0('reports/', country.code, '.pdf')
-system(paste('mv mortality.pdf', pdf.path))
+compile <- function(compilation.id, config) {
+    set.knitr.defaults()
+    rmd.path <- paste0('templates/', config$template, '.Rmd')
+    knit2html(rmd.path, quiet=TRUE)
+
+    html.path <- paste0(config$template, '.html')
+    destination <- paste0('reports/', compilation.id, '.html')
+    system(paste('mv', html.path, destination))
+
+    md.path <- paste0(config$template, '.md')
+    system(paste('rm', md.path))
+}
+
+get.data <- function(config) {
+    return(do.call(extract, config$data))
+}
+
+## TODO: This JSON parser doesn't throw errors when it should.
+compilations <- fromJSON(file='compilations.json')
+for (k in names(compilations)) {
+    compile(k, compilations[[k]])
+}
+
+md2pdf <- function() {
+    ## Let's also make a pdf using pandoc.
+    system('pandoc mortality.md -o mortality.pdf')
+    pdf.path <- paste0('reports/', country.code, '.pdf')
+    system(paste('mv mortality.pdf', pdf.path))
+}
