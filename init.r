87eae5ca9ab30596ea5da48200d1b5bab2c476a8
Andrew Marder <andrew.n.marder@gmail.com>
2014-05-30 15:24:35 -0400

    Let's see if this sets up R correctly.

---
+++ init.r
@@ -0,0 +1,12 @@
+requirements <- c(
+    'knitr',
+    'rjson',
+    'pander',
+    'ggplot2',
+    'plyr',
+    'reshape2',
+    )
+
+for (lib in requirements) {
+    install.packages(lib, dependencies=TRUE)
+}
