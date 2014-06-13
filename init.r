fa76e206d6f4b14ddead5a8f5de04229e1b80909
Andrew Marder <andrew.n.marder@gmail.com>
2014-06-13 07:54:17 -0400

    Set up install for rCharts.

--- init.r
+++ init.r
@@ -4,9 +4,28 @@ requirements <- c(
     'pander',
     'ggplot2',
     'plyr',
-    'reshape2'
+    'reshape2',
     )
 
-for (lib in requirements) {
-    install.packages(lib, dependencies=TRUE)
+init <- function() {
+    install.list(requirements)
+    install.rcharts()
 }
+
+install.list <- function(l) {
+    for (lib in requirements) {
+        install.packages(lib, dependencies=TRUE)
+    }
+}
+
+install.rcharts <- function() {
+    rcharts.deps <- c('RCurl', 'RJSONIO', 'whisker', 'yaml')
+    install.list(rcharts.deps)
+    url <- 'https://codeload.github.com/ramnathv/rCharts/tar.gz/master'
+    path <- 'rCharts.tar.gz'
+    system(paste('curl', url, '-o', path))
+    install.packages(path, repos=NULL, type='source')
+    system(paste('rm', path))
+}
+
+init()
