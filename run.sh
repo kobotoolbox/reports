b49cdc8b6e3d01b8f2dc2f1c2557cd304cc9cbaa
Andrew Marder <andrew.n.marder@gmail.com>
2015-07-22 20:22:25 -0400

    Increased timeout. It would probably be good to use celery to compile reports in the backend.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ python manage.py syncdb --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn --bind 0.0.0.0:5000 koboreports.wsgi
+gunicorn --bind 0.0.0.0:5000 --timeout 300 koboreports.wsgi
