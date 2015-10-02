1225ff05981a55110601a801194bdaa4b6aa826a
pmusaraj <pmusaraj@gmail.com>
2015-10-02 09:55:50 -0400

    2-column layout for reports for non-mobile sizes

--- demo/src/components/demoReportHtml.js
+++ demo/src/components/demoReportHtml.js
@@ -4,37 +4,46 @@ var demoReportHtml = `
     <h3>Wealth Quintile Results</h3>
     <p>2 respondents have been interviewed.</p>
     <p>Warning: Your sample is too small. Collect at least 100 responses to be representative.</p>
-    <div id="national-quintile-results" class="section level2">
-      <h3>National quintile results</h3>
-      <p>These are the national quintile results for these respondents:</p>
-      <table class="table table-condensed">
-        <caption>Respondents by wealth quintile</caption>
-        <thead>
-          <tr class="header">
-            <th align="left">Wealth quintile</th>
-            <th align="left">Percentage of respondents</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="odd">
-            <td align="left">4</td>
-            <td align="left">100</td>
-          </tr>
-          <tr class="even">
-            <td align="left">Total</td>
-            <td align="left">100</td>
-          </tr>
-        </tbody>
-      </table>
-      <p>
+    <div id="national-quintile-results" class="section level2 columns">
+      <div class="column-desc">
+        <h3>National quintile results</h3>
+        <p>These are the national quintile results for these respondents:</p>
+        <p>The table above shows you the percentage of your respondents that were in each national wealth quintile.</p>
+        <p>0% of your respondents were in the bottom two quintiles. This means that a relatively small number of respondents were poor. For more information on interpreting wealth quintile results, <a href="http://www.equitytool.org/wealth-quintiles/">please click here</a>.</p>
+      </div>
+
+      <div class="column-graph">
         <img src="${require('../images/sample_graph.png')}" />
-      </p>
-      <p>The table above shows you the percentage of your respondents that were in each national wealth quintile.</p>
-      <p>0% of your respondents were in the bottom two quintiles. This means that a relatively small number of respondents were poor. For more information on interpreting wealth quintile results, <a href="http://www.equitytool.org/wealth-quintiles/">please click here</a>.</p>
+        <table class="table table-condensed">
+          <caption>Respondents by wealth quintile</caption>
+          <thead>
+            <tr class="header">
+              <th align="left">Wealth quintile</th>
+              <th align="left">Percentage of respondents</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="odd">
+              <td align="left">4</td>
+              <td align="left">100</td>
+            </tr>
+            <tr class="even">
+              <td align="left">Total</td>
+              <td align="left">100</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div id="urban-quintile-results" class="section level2">
-      <h2>Urban quintile results</h2>
-      <p>These are the urban quintile results for these respondents:</p>
+    <div id="urban-quintile-results" class="section level2 columns">
+      <div class="column-desc">
+        <h3>Urban quintile results</h3>
+        <p>These are the urban quintile results for these respondents:</p>
+        <p>The table above shows you the percentage of your respondents that were in each urban wealth quintile.</p>
+        <p>100% of your respondents were in the bottom two quintiles. This means that a relatively large number of respondents were poor. For more information on interpreting wealth quintile results, <a href="http://www.equitytool.org/wealth-quintiles/">please click here</a>.</p>
+      </div>
+      <div class="column-graph">
+        <img src="${require('../images/sample_graph.png')}" />
         <table class="table table-condensed">
           <caption>Respondents by wealth quintile</caption>
           <thead>
@@ -54,10 +63,7 @@ var demoReportHtml = `
             </tr>
           </tbody>
         </table>
-        <img src="${require('../images/sample_graph.png')}" />
-      </p>
-      <p>The table above shows you the percentage of your respondents that were in each urban wealth quintile.</p>
-      <p>100% of your respondents were in the bottom two quintiles. This means that a relatively large number of respondents were poor. For more information on interpreting wealth quintile results, <a href="http://www.equitytool.org/wealth-quintiles/">please click here</a>.</p>
+      </div>
     </div>
   </div>
 </div>
