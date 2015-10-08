75c8e9aeea876b4e209e523ef717ee8a8a8c6e97
Alex Dorey <dorey415@gmail.com>
2015-10-08 06:49:22 -0400

    projects is undefined

--- demo/src/stores/renderings.js
+++ demo/src/stores/renderings.js
@@ -13,7 +13,7 @@ var projects = [
 var renderingsStore = Reflux.createStore({
   init () {
     this.state = {
-      projects: projects,
+      projects: [],
     };
     this.listenTo(actions.listRenderings.completed, this.listRenderingsCompleted);
     this.listenTo(actions.listRenderings.failed, this.listRenderingsFailed);
