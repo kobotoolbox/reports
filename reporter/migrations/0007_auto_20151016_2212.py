c7baf0d57f85e253bdf6c4c5285f0acd676655df
Andrew Marder <andrew.n.marder@gmail.com>
2015-10-16 19:20:27 -0400

    Added migrations that probably should have been added earlier with the corresponding changes to the models.

---
+++ reporter/migrations/0007_auto_20151016_2212.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reporter', '0006_auto_20150910_1721'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='template',
+            name='user',
+        ),
+        migrations.AddField(
+            model_name='template',
+            name='name',
+            field=models.TextField(default=b''),
+        ),
+    ]
