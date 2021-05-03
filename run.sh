54f78444a76cd6128c759339c1f8ceb79027f13e
John N. Milner <john@tmoj.net>
2021-05-03 02:25:58 -0400

    Fix startup script
    …which was failing with `ValueError: Empty module name` in production
    mode

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
+# This script should be run inside a Docker container;
+# see the `CMD` line in the `Dockerfile`
+
 source activate koboreports
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Enable Gunicorn auto-reloading if DEBUG=True (case insensitive)
-[ "${DEBUG,,}" == 'true' ] && AUTORELOAD='--reload' || AUTORELOAD=''
+if [ "${DEBUG,,}" == 'true' ]
+then
+    gunicorn --bind=0.0.0.0:$PORT --workers=3 --timeout=300 --reload koboreports.wsgi
+else
+    # You must set the NGINX `proxy_read_timeout` to something greater than or
+    # equal to the Gunicorn `--timeout` value specified here
+    gunicorn --bind=0.0.0.0:$PORT --workers=3 --timeout=300 koboreports.wsgi
+fi
 
-# you probably need to set the NGINX `proxy_read_timeout` for `--timeout=300`
-# to be of any use
-gunicorn --bind=0.0.0.0:$PORT --workers=3 --timeout=300 "$AUTORELOAD" koboreports.wsgi
